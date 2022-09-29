2ed364176aebf2c04028746ba55428608b0740dd
ramon <a21rambertor@inspedralbes.cat>
2022-09-29 12:24:53 +0200

    1r canvi

--- app/src/main/java/com/example/exercici5berram/MainActivity.java
+++ app/src/main/java/com/example/exercici5berram/MainActivity.java
@@ -6,12 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mMessageEditText;
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.twoactivities.extra.MESSAGE";
+
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mMessageEditText = findViewById(R.id.editText_main);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
@@ -20,9 +26,8 @@ public class MainActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
-
-
-
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
     }
 
 
